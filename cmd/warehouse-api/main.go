590cf3f80646428d2b0d924cbb7ed568ce5e596f
CristianQS <cristianqs31@gmail.com>
2024-09-30 00:56:42 +0100

    add Mux to handle APIs

--- cmd/warehouse-api/main.go
+++ cmd/warehouse-api/main.go
@@ -1,1 +1,21 @@
-package warehouseapi
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	productHandler := ProductHandler{}
+	r.HandleFunc("/warehouses/{id}", productHandler.ServeHttp)
+	http.ListenAndServe(":8080", r)
+}
+
+type ProductHandler struct{}
+
+func (*ProductHandler) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("This is the API warehouse.You want: %s", mux.Vars(r)["id"])))
+}
