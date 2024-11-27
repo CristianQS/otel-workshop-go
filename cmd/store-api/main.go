1a757eb1d1b5b0d3aecb787d950b1679f3ce6ce5
CristianQS <cristianqs31@gmail.com>
2024-11-27 00:32:28 +0000

    fix refactor

--- cmd/store-api/main.go
+++ cmd/store-api/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/CristianQS/otel-workshop-go/pkg/clients"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	productHandler := internal.handlers.ProductHandler{}
+	productHandler := clients.ProductHandler{}
 	r.HandleFunc("/products/{id}", productHandler.GetProductById).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
